da16400a45588b8026139713381a4d26eae56c26
Mayank Shekhar <mshekhar949@gmail.com>
2023-02-10 15:54:05 +0530

    RWOnly first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        RWOnly b1 = new RWOnly();
+//        b1.name = "Ramesh";
+//        System.out.println(b1.name);
+//        name' has private access in 'com.driver.RWOnly'
+        b1.setName("Ramesh Kumar Gill");
+        System.out.print(b1.getName());
+    }
 }
